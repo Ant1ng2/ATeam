4fd7f79ef614e4467b1fb14a5c53656381f42c6f
Anthony <acling2k@gmail.com>
2018-08-25 14:56:31 -0700

    Anthony's attempt in creating a Game object. Abstraction may be implemented in further projects.

--- game.py
+++ game.py
@@ -1,1 +1,21 @@
-def homework:
+class Game:
+
+    def __init__(self):
+        self.position = 10
+
+    def setPosition(self, position):
+        self.position = position
+
+    def doMove(self, move):
+        self.position = self.position - move
+
+    def generateMoves(self):
+        return [x + 1 for x in range(2) if x + 1 <= self.position]
+
+    def primative(self):
+        if self.position == 0:
+            return -1
+        if self.position == 1:
+            return 1
+        else:
+            return 0
