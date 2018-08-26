c73613ec495d6895d0e41ccab1ef05bd982fbc96
csbo <csbo@outlook.com>
2018-08-25 20:36:41 -0700

    function solve(P)

--- game.py
+++ game.py
@@ -1,21 +1,30 @@
 class Game:
 
-    def __init__(self):
-        self.position = 10
+    def doMove(self, position, move):
+        return position - move
 
-    def setPosition(self, position):
-        self.position = position
+    def generateMoves(self, position):
+        return [x + 1 for x in range(2) if x + 1 <= position]
 
-    def doMove(self, move):
-        self.position = self.position - move
-
-    def generateMoves(self):
-        return [x + 1 for x in range(2) if x + 1 <= self.position]
-
-    def primative(self):
-        if self.position == 0:
+    def primitive(self, position):
+        '''
+        Get primitive value
+        -1: lose; 1: win; 0: undecided
+        '''
+        if position == 0:
             return -1
-        if self.position == 1:
-            return 1
         else:
             return 0
+
+    def solve(self, position):
+        for move in self.generateMoves(position):
+            newPosition = self.doMove(position, move)
+            primitiveValue = self.primitive(newPosition)
+            if primitiveValue == -1 or (primitiveValue == 0 and self.solve(newPosition) == -1):
+                return 1
+        else:
+            return -1
+
+game = Game()
+for i in range(11):
+    print(i, game.solve(i))
